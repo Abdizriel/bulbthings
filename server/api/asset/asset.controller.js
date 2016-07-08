ff997270f2785b70a200a2101fccd5695412e6c7
Marcin Mrotek <marcin.mrotek@stv.tv>
2016-07-08 14:05:09 +0100

    [UPDATE] Moved API response functions to separate file

--- server/api/asset/asset.controller.js
+++ server/api/asset/asset.controller.js
@@ -14,77 +14,9 @@
 import { Asset } from '../../config/db.conf.js';
 
 /**
- * @function respondWithResult
- * @description Function that returns response with data
- * @param {Object} res - Express Framework Response Object
- * @param {Number} statusCode - Response status code
+ * @description API Response Utils
  */
-function respondWithResult(res, statusCode) {
-  statusCode = statusCode || 200;
-  return function(entity) {
-    if (entity) {
-      res.status(statusCode).json(entity);
-    }
-  };
-}
-
-/**
- * @function saveUpdates
- * @description Function that updates entity with new data
- * @param {Object} updates - Updated data
- */
-function saveUpdates(updates) {
-  return function(entity) {
-    return entity.updateAttributes(updates)
-        .then(updated => {
-          return updated;
-        });
-  };
-}
-
-/**
- * @function removeEntity
- * @description Function that remove entity from Schema
- * @param {Object} res - Express Framework Response Object
- */
-function removeEntity(res) {
-  return function(entity) {
-    if (entity) {
-      return entity.destroy()
-          .then(() => {
-            res.status(204).end();
-          });
-    }
-  };
-}
-
-/**
- * @function handleEntityNotFound
- * @description Function that handle entity not found respond
- * @param {Object} res - Express Framework Response Object
- */
-function handleEntityNotFound(res) {
-  return function(entity) {
-    if (!entity) {
-      res.status(404).end();
-      return null;
-    }
-    return entity;
-  };
-}
-
-/**
- * @function handleError
- * @description Function that returns response with error details
- * @param {Object} res - Express Framework Response Object
- * @param {Number} statusCode - Response status code
- */
-function handleError(res, statusCode) {
-  statusCode = statusCode || 500;
-  return function(err) {
-    res.status(statusCode).send(err);
-  };
-}
+import { handleError, handleEntityNotFound, removeEntity, saveUpdates, respondWithResult } from '../utils';
 
 /**
  * @function index
---
+++ server/api/utils.js
@@ -0,0 +1,80 @@
+/**
+ * @function respondWithResult
+ * @description Function that returns response with data
+ * @param {Object} res - Express Framework Response Object
+ * @param {Number} statusCode - Response status code
+ */
+function respondWithResult(res, statusCode) {
+  statusCode = statusCode || 200;
+  return function(entity) {
+    if (entity) {
+      res.status(statusCode).json(entity);
+    }
+  };
+}
+
+/**
+ * @function saveUpdates
+ * @description Function that updates entity with new data
+ * @param {Object} updates - Updated data
+ */
+function saveUpdates(updates) {
+  return function(entity) {
+    return entity.updateAttributes(updates)
+      .then(updated => {
+        return updated;
+      });
+  };
+}
+
+/**
+ * @function removeEntity
+ * @description Function that remove entity from Schema
+ * @param {Object} res - Express Framework Response Object
+ */
+function removeEntity(res) {
+  return function(entity) {
+    if (entity) {
+      return entity.destroy()
+        .then(() => {
+          res.status(204).end();
+        });
+    }
+  };
+}
+
+/**
+ * @function handleEntityNotFound
+ * @description Function that handle entity not found respond
+ * @param {Object} res - Express Framework Response Object
+ */
+function handleEntityNotFound(res) {
+  return function(entity) {
+    if (!entity) {
+      res.status(404).end();
+      return null;
+    }
+    return entity;
+  };
+}
+
+/**
+ * @function handleError
+ * @description Function that returns response with error details
+ * @param {Object} res - Express Framework Response Object
+ * @param {Number} statusCode - Response status code
+ */
+function handleError(res, statusCode) {
+  statusCode = statusCode || 500;
+  return function(err) {
+    res.status(statusCode).send(err);
+  };
+}
+
+export default {
+  handleError,
+  handleEntityNotFound,
+  removeEntity,
+  saveUpdates,
+  respondWithResult
+}
