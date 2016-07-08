c6af87bec276ffe0cab37f90a7ac05c40f7396e6
Marcin Mrotek <marcin.mrotek@stv.tv>
2016-07-08 12:44:23 +0100

    [FIX] Documentation in User Service

--- user-service/server/api/user/user.controller.js
+++ user-service/server/api/user/user.controller.js
@@ -88,7 +88,7 @@ function handleError(res, statusCode) {
 
 /**
  * @function index
- * @description Function that returns all services from Service Data Access Object
+ * @description Function that returns all users
  * @param {Object} req - Express Framework Request Object
  * @param {Object} res - Express Framework Response Object
  */
@@ -100,8 +100,7 @@ function index(req, res) {
 
 /**
  * @function show
- * @description Function that returns single service from Service Data Access Object
- * by id provided in url
+ * @description Function that returns single user by id provided in url
  * @param {Object} req - Express Framework Request Object
  * @param {Object} res - Express Framework Response Object
  */
@@ -118,8 +117,7 @@ function show(req, res) {
 
 /**
  * @function create
- * @description Function that create service through Service Data Access Object
- * by provided request body
+ * @description Function that create user by provided request body
  * @param {Object} req - Express Framework Request Object
  * @param {Object} res - Express Framework Response Object
  */
@@ -131,8 +129,7 @@ function create(req, res) {
 
 /**
  * @function update
- * @description Function that update service through Service Data Access Object
- * by provided id in url and updated data in request body
+ * @description Function that update user by provided id in url and updated data in request body
  * @param {Request} req - Express Framework Request Object
  * @param {Response} res - Express Framework Response Object
  */
@@ -153,8 +150,7 @@ function update(req, res) {
 
 /**
  * @function destroy
- * @description Function that delete service through Service Data Access Object
- * by id provided in url
+ * @description Function that delete user by id provided in url
  * @param {Object} req - Express Framework Request Object
  * @param {Object} res - Express Framework Response Object
  */
